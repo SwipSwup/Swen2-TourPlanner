19f949db2bf55ab61293768f2df41229529c970a
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2025-03-17 18:41:27 +0100

    added UserControlls

---
+++ UI/Resources/UserControlls/ConfirmCancelButtons.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TourPlanner.UserControls
+{
+    public partial class ConfirmCancelButtons : UserControl
+    {
+        public ConfirmCancelButtons()
+        {
+            InitializeComponent();
+        }
+
+        // Event handlers that you will raise (forward) to the parent
+        public event RoutedEventHandler OkButtonClicked;
+        public event RoutedEventHandler CancelButtonClicked;
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Raise event to parent
+            OkButtonClicked?.Invoke(this, e);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Raise event to parent
+            CancelButtonClicked?.Invoke(this, e);
+        }
+    }
+}
---
+++ UI/Resources/UserControlls/TourFormControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TourPlanner.UserControls
+{
+    public partial class TourFormControl : UserControl
+    {
+        public TourFormControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
